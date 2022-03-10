ec476813d0041a3bc581fe476672bedb5a286784
Chanin Nusang <chanin.nusang@gmail.com>
2022-03-10 16:39:39 +0700

    PollSystem

---
+++ BasicASPRazor/PollSystem/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicASPRazor.PollSystem
+{
+    public class Item
+    {
+        public string label { get; set; }
+        public string value { get; set; }
+    }
+}
